56f014e7f78d691b092516f52ba4ddbc8aa5add8
Piers Chambers <piers@varyonic.com>
2018-12-21 14:10:08 -0500

    Implement hmac_authorization

--- lib/amex_tokenization_client.rb
+++ lib/amex_tokenization_client.rb
@@ -3,10 +3,22 @@ require 'openssl'
 require "amex_tokenization_client/version"
 
 class AmexTokenizationClient
-  attr_reader :client_secret
+  attr_reader :host
+  attr_reader :client_id, :client_secret
 
-  def initialize(client_secret:)
-    @client_secret = client_secret
+  def initialize(host:, client_id:, client_secret:)
+    @host = host
+    @client_id, @client_secret = client_id, client_secret
+  end
+
+  # @param [String] method, e.g. 'POST'
+  # @param [String] resource_path, e.g. '/payments/digital/v2/tokens/provisioning'
+  # @param [String] JSON payload
+  # @return [String] Authorization: MAC id="gfFb4K8esqZgMpzwF9SXzKLCCbPYV8bR",ts="1463772177193",nonce="61129a8d-ca24-464b-8891-9251501d86f0", bodyhash="YJpz6NdGP0aV6aYaa+6qKCjQt46of+Cj4liBz90G6X8=", mac="uzybzLPj3fD8eBZaBzb4E7pZs+l+IWS0w/w2wwsExdo="
+  def hmac_authorization(method, resource_path, payload, nonce = SecureRandom.uuid, ts = (Time.now.to_r * 1000).to_i)
+    bodyhash = hmac_digest(payload)
+    mac = hmac_digest([ts, nonce, method, resource_path, host, 443, bodyhash, ''].join("\n"))
+    %(MAC id="#{client_id}",ts="#{ts}",nonce="#{nonce}",bodyhash="#{bodyhash}",mac="#{mac}")
   end
 
   def hmac_digest(s)
--- spec/amex_tokenization_client_spec.rb
+++ spec/amex_tokenization_client_spec.rb
@@ -3,20 +3,42 @@ RSpec.describe AmexTokenizationClient do
     expect(AmexTokenizationClient::VERSION).not_to be nil
   end
 
+  let(:host) { 'api.qa.americanexpress.com' }
+  let(:client_id) { ENV.fetch('AETS_CLIENT_ID') }
   let(:client_secret) { ENV.fetch('AETS_CLIENT_SECRET') }
+
   subject do
     AmexTokenizationClient.new(
+      host: host,
+      client_id: client_id,
       client_secret: client_secret
     )
   end
 
   context do
+    # Copied from amex-api-java-client-core ApiAuthenticationTest.java
+    let(:host) { 'github.com' }
+    let(:client_id) { "UNIT-TEST-KEY-4388-87b9-85cf463231d7" }
     let(:client_secret) { 'UNIT-TEST-SEC-4206-8a21-a73eed54c896' }
     let(:payload) { 'The swift brown fox jumped over the lazy dogs back' }
     let(:bodyhash) { 'wlAalPXGd1oDuqepWDawftGy9zhgEV3oHZve/hz5Yac=' }
 
     it 'digests a payload' do
       expect(subject.hmac_digest(payload)).to eq(bodyhash)
+    end
+
+    context do
+      let(:resource_path) { "/americanexpress/risk/fraud/v1/enhanced_authorizations/online_purchases" }
+      let(:nonce) { "f00870f3-5862-45f1-9bd1-ba94c71d2661"}
+      let(:ts) { "1473803713478" }
+
+      it 'generates an HMAC header value' do
+        authorization = subject.hmac_authorization('POST', resource_path, payload, nonce, ts)
+        expect(authorization).to match(%Q{MAC id="#{client_id}"})
+        expect(authorization).to match(%Q{,ts="#{ts}"})
+        expect(authorization).to match(%Q{,nonce="#{nonce}"})
+        expect(authorization).to match(%Q{,bodyhash="#{bodyhash}"})
+      end
     end
   end
 end
